6f0cff388a347fc625c7331d03331a948147c47f
rochevin <vincent.rocher.armd@gmail.com>
2019-07-15 12:51:13 +0200

    Add comments

--- R/Get1val.R
+++ R/Get1val.R
@@ -1,18 +1,18 @@
 #' Title
 #'
-#' @param my.wigs
-#' @param one.w
-#' @param x
+#' @param Name The name of the bigwig file
+#' @param one.w A bigwig file imported in RleList format (as = "RleList")
+#' @param x A GRanges object
 #'
-#' @return
+#' @return A tibble with the coverage of each element of x
 #' @export
 #'
 #' @examples
-Get1val <- function(my.wigs,one.w,x){
+Get1val <- function(Name,one.w,x){
     lapply(split(x,droplevels(seqnames(x))),function(zz){
         message(unique(as.character(seqnames(zz))))
         cov <- one.w[[unique(as.character(seqnames(zz)))]]
         score <- Views( cov, start = start(zz), end = end(zz) ) %>% sum()
-        tibble(wig = my.wigs,value = score,rowname = zz$name)
+        tibble(wig = Name,value = score,rowname = zz$name)
     }) %>% bind_rows()
 }
